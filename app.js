5b99ffd5dcbbe38e35dff84d2907206b6e7efed8
abhinav singh <singhabhinav083@gmail.com>
2020-06-26 19:27:49 +0530

    dbc

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var bodyParser=require("body-parser");
 app.use(methodOverride('_method'))
 
 //mongoose.connect("mongodb://localhost/dofos",{useNewUrlParser: true,useUnifiedTopology: true });
-mongoose.connect("mongodb+srv://abhinav:KonmIeixRZ0cyhFI@cluster0-yebob.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true,useUnifiedTopology: true })
-//mongoose.connect("mongodb+srv://abhinav:KonmIeixRZ0cyhFI@cluster0-yebob.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true})
+//mongoose.connect("mongodb+srv://abhinav:KonmIeixRZ0cyhFI@cluster0-yebob.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true,useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://abhinav:bJoIkOywrOXRdUpr@cluster0-yebob.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true})
 
 
 
