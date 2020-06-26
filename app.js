0be4be5e62aa528403fe3c31c7e9774edca4e68e
abhinav singh <singhabhinav083@gmail.com>
2020-06-26 19:37:13 +0530

    dbfc

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.use(methodOverride('_method'))
 
 //mongoose.connect("mongodb://localhost/dofos",{useNewUrlParser: true,useUnifiedTopology: true });
 //mongoose.connect("mongodb+srv://abhinav:KonmIeixRZ0cyhFI@cluster0-yebob.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true,useUnifiedTopology: true })
-mongoose.connect("mongodb+srv://abhinav:bJoIkOywrOXRdUpr@cluster0-yebob.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true,useUnifiedTopology: true })
+//mongoose.connect("mongodb+srv://abhinav:bJoIkOywrOXRdUpr@cluster0-yebob.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true,useUnifiedTopology: true })
 
-
+mongoose.connect("mongodb+srv://abhinav:bJoIkOywrOXRdUpr@cluster0-yebob.mongodb.net/test?retryWrites=true&w=majority"{useNewUrlParser:true,useUnifiedTopology:true})
 
 app.use(express.static(__dirname + '/'));
 
